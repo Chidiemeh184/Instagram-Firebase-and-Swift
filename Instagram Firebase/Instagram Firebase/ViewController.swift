c76cbdac3b54842d3ad57248766ee1c160fd52e7
Chidi Emeh <chidiemeh184@gmail.com>
2017-06-06 11:01:29 -0400

    Created File

---
+++ Instagram Firebase/Instagram Firebase/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Instagram Firebase
+//
+//  Created by Chidi Emeh on 6/6/17.
+//  Copyright © 2017 Chidi Emeh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
