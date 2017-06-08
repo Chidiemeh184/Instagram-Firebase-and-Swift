33157ce580bfd9822473efbbad5234f8d84eb5bc
Chidi Emeh <chidiemeh184@gmail.com>
2017-06-07 21:51:48 -0400

    Refactor Storyboard and Reference

--- Instagram Firebase/Instagram Firebase/SignUpViewController.swift
+++ Instagram Firebase/Instagram Firebase/SignUpViewController.swift
@@ -10,10 +10,44 @@ import UIKit
 
 class SignUpViewController: UIViewController {
 
+    @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var profileImage: UIImageView!
+    
+    //First Loading Function
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        usernameTextField.backgroundColor = .clear
+        usernameTextField.tintColor = .white
+        usernameTextField.textColor = .white
+        usernameTextField.attributedPlaceholder = NSAttributedString(string: usernameTextField.placeholder!, attributes: [NSForegroundColorAttributeName: UIColor(white: 1.0, alpha: 0.6)])
+        let bottomLayerUsername = CALayer()
+        bottomLayerUsername.frame = CGRect(x: 0, y: 29, width: 1000, height: 0.6)
+        bottomLayerUsername.backgroundColor = UIColor(red: 50/255, green: 50/255, blue: 25/255, alpha: 1).cgColor
+        usernameTextField.layer.addSublayer(bottomLayerUsername)
 
-        print("Welcome Sign Up ")
+        emailTextField.backgroundColor = .clear
+        emailTextField.tintColor = .white
+        emailTextField.textColor = .white
+        emailTextField.attributedPlaceholder = NSAttributedString(string: emailTextField.placeholder!, attributes: [NSForegroundColorAttributeName: UIColor(white: 1.0, alpha: 0.6)])
+        let bottomLayerEmail = CALayer()
+        bottomLayerEmail.frame = CGRect(x: 0, y: 29, width: 1000, height: 0.6)
+        bottomLayerEmail.backgroundColor = UIColor(red: 50/255, green: 50/255, blue: 25/255, alpha: 1).cgColor
+        emailTextField.layer.addSublayer(bottomLayerEmail)
+        
+        passwordTextField.backgroundColor = .clear
+        passwordTextField.tintColor = .white
+        passwordTextField.textColor = .white
+        passwordTextField.attributedPlaceholder = NSAttributedString(string: passwordTextField.placeholder!, attributes: [NSForegroundColorAttributeName: UIColor(white: 1.0, alpha: 0.6)])
+        let bottomLayerPassword = CALayer()
+        bottomLayerPassword.frame = CGRect(x: 0, y: 29, width: 1000, height: 0.6)
+        bottomLayerPassword.backgroundColor = UIColor(red: 50/255, green: 50/255, blue: 25/255, alpha: 1).cgColor
+        passwordTextField.layer.addSublayer(bottomLayerPassword)
+
+        profileImage.layer.cornerRadius = 10
+        profileImage.clipsToBounds = true
     }
 
     override func didReceiveMemoryWarning() {
@@ -25,7 +59,4 @@ class SignUpViewController: UIViewController {
         dismiss(animated: true, completion: nil)
     }
     
-    deinit {
-        print("Sign up was removed from memory")
-    }
 }
